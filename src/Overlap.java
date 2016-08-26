1284731e325c55b43b6dacab5e4f6c50f56f1745
SeanRice1 <seanrice2015@gmail.com>
2016-08-26 18:27:35 -0500

    Improved compare algorithm

--- src/Overlap.java
+++ src/Overlap.java
@@ -1,23 +1,45 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Overlap {
-	private static ArrayList<String> results = new ArrayList<>();
+	private static HashMap<Integer, String> results = new HashMap<>();
 
 	public static String checkOverlap() {
 		System.out.println("Checking...");
 		if (Info.listOfCompounds.size() > 1) {
 
 			for (int x = 0; x < Info.listOfCompounds.size() - 1; x++) {
-				for (int y = x + 1; y < Info.listOfCompounds.size(); y++) {
-					ArrayList<String> arr1 = Info.listOfCompounds.get(x).getAidList();
-					ArrayList<String> arr2 = Info.listOfCompounds.get(y).getAidList();
-					for (int z = 0; z < arr1.size(); z++) {
-						for (int a = 0; a < arr2.size(); a++) {
-							if (arr1.get(z).equals(arr2.get(a))) {
-								results.add("| The Bioassay AID number: " + arr1.get(z) + " is in common in both "
-										+ Info.listOfCompounds.get(x).getName() + " and "
-										+ Info.listOfCompounds.get(y).getName()+ " |");
+				ArrayList<String> arr1 = Info.listOfCompounds.get(x).getAidList();
+				ArrayList<String> arr2 = Info.listOfCompounds.get(x + 1).getAidList();
+				for (int z = 0; z < arr1.size(); z++) {
+					boolean alreadyDone = false;
+					for (int a = 0; a < arr2.size(); a++) {
+
+						if (arr1.get(z).equals(arr2.get(a))) {
+							if (!alreadyDone) {
+
+								if (!results.containsKey(Integer.parseInt(arr1.get(z)))) {
+									results.put(Integer.parseInt(arr1.get(z)),
+											" is mutual between " + Info.listOfCompounds.get(x).getName() + ", "
+													+ Info.listOfCompounds.get(x + 1).getName());
+								} else {
+									String temp = results.get(Integer.parseInt(arr1.get(z)));
+									results.remove(Integer.parseInt(arr1.get(z)));
+									results.put(Integer.parseInt(arr2.get(a)),
+											temp + ", " + Info.listOfCompounds.get(x + 1).getName());
+								}
+
+								alreadyDone = true;
 							}
+
+							else {
+								String temp = results.get(Integer.parseInt(arr1.get(z)));
+								results.remove(Integer.parseInt(arr1.get(z)));
+								results.put(Integer.parseInt(arr2.get(a)),
+										temp + ", " + Info.listOfCompounds.get(x + 1).getName());
+							}
+
 						}
 					}
 				}
